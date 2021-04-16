e7cc78f38446bc0a2025b3f260d689fb4520ac51
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-16 20:46:11 -0300

    feat(theme): add typography variant in themes

---
+++ src/theme/typographyVariants.ts
@@ -0,0 +1,22 @@
+export const typographyVariants = {
+  title: {
+    fontSize: '72px',
+    fontWeight: '400',
+    lineHeight: 2, // 200%
+  },
+  titleXS: {
+    fontSize: '36px',
+    fontWeight: '400',
+    lineHeight: 1.5, // 150%
+  },
+  subTitle: {
+    fontSize: '24px',
+    fontWeight: '400',
+    lineHeight: 1.25, // 125%
+  },
+  paragraph: {
+    fontSize: '18px',
+    fontWeight: '400',
+    lineHeight: 1, // 100%
+  },
+};
