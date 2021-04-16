ce4bc3df17303d63651daadda742df0fd2831641
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-16 20:48:14 -0300

    feat(theme): config default color pallete

--- @types/styled.d.ts
+++ @types/styled.d.ts
@@ -1,9 +1,13 @@
 import 'styled-components';
-import { ColorsType } from '../src/types/colorsType.ts';
+import { ColorsType } from '../src/types/colorsType';
+import { BreakpointsType } from '../src/types/breakpointsType';
+import { TypographyVariantsType } from '../src/types/typographyVariantsType';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: ColorsType,
+    typographyVariants: TypographyVariantsType,
+    breakpoints: BreakpointsType,
     transition: string;
     fontFamily: string;
   }
