deb1496ecca1a9211a5da3551237e6d69c85d25e
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-16 21:16:16 -0300

    refactor(pages): change the _app.tsx by decentralizing core app functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../src/theme/GlobalStyle';
-
-import { theme } from '../src/theme';
+import { Head } from '@/components/commons/Head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+    <>
+      <Head />
       <Component {...pageProps} />
-    </ThemeProvider>
+    </>
   );
 }
